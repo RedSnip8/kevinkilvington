d5e7285ec2f4b130c191dc3e0eff0bacdb19d6e0
Redsnip8 <Fcipolone@gmail.com>
2021-02-09 21:10:22 -0500

    Page headers added and albums main page

--- kevinkilvingtonsite/albums/views.py
+++ kevinkilvingtonsite/albums/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_list_or_404
 
 from .models import Album
+from home.models import Picture
 
 
 def albums(request):
-    albums_list = Album.objects.filter(is_active=True)
-    context = {"albums_list": albums_list}
+    cover_photo = Picture.objects.filter(on_grid=True).select_related('photo_album').distinct("photo_album").order_by("photo_album", "-id")
+
+    context = { "cover_photo": cover_photo }
 
     return render(request, 'albums/albums.html', context)
 
