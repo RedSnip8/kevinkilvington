ec38ce4a76d363e8dde0dc62728ddc4a997b6c0b
Redsnip8 <Fcipolone@gmail.com>
2021-02-11 19:38:43 -0500

    Finalized albums app and added breadcrub

--- kevinkilvingtonsite/albums/urls.py
+++ kevinkilvingtonsite/albums/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.albums, name='albums'),
     path('<int:album_id>', views.events, name='events'),
-    path('<int:event_id>', views.event, name='event')
+    path('events/<int:event_id>', views.event, name='event')
 ]
